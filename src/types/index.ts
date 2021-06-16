20c67a12add6308f9005ce522d8f953a00056a76
leandrocmaia <64499986+leandrocmaia@users.noreply.github.com>
2021-06-16 09:49:41 +0200

    refactor and added tests (#2)

--- src/types/index.ts
+++ src/types/index.ts
@@ -67,68 +67,13 @@ export class Transaction implements ITransaction {
 export interface IBlockChain {
   blocks: IBlock[];
   genesisBlock: IBlock;
-  addBlock(block: IBlock): void;
-  buildGenesisBlock(block: IBlock): IBlock;
-  getPreviousBlock(): IBlock;
-  getNextBlock(transactions: ITransaction[]): IBlock;
-  generateHash(block: IBlock): string;
 }
 
 export class BlockChain implements IBlockChain {
-  public blocks: IBlock[];
-  public genesisBlock: IBlock;
-  
-  constructor(genesisBlock: IBlock) {
-    this.blocks = []
-    this.genesisBlock = this.buildGenesisBlock(genesisBlock);
-  }
-  
-  buildGenesisBlock(block: IBlock): IBlock {
-    block.previousHash = "00000000000000";
-    block.hash = this.generateHash(block);
-    this.addBlock(block);
-    return block;
-  }
-
-  getPreviousBlock(): IBlock {
-    return this.blocks[this.blocks.length - 1]
-  }
-  
-  getNextBlock(transactions: ITransaction[]): IBlock {
-    let block = new Block();
-    transactions.map((t: ITransaction) => {
-      block.addTransaction(t);
-    });
-
-    let previousBlock = this.getPreviousBlock();
-    block.index = this.blocks.length;
-    block.previousHash = previousBlock.hash;
-    block.hash = this.generateHash(block);
-    return block;
-  }
-
-  public addBlock(block: IBlock): void {
-    this.blocks = [...this.blocks, block];
-  }
-
-  public generateHash(block: IBlock): string {
-    let hash = sha256(block.key);
-    let difficult = Array(block.difficulty + 1).join("0");
-    // mining
-    process.stdout.write("Mining ");
-    const now = Date.now();
-    while(!hash.startsWith(difficult)) {
-      block.nonce += 1;
-      hash = sha256(block.key);
-      if (block.nonce % 500 == 0) {
-        process.stdout.write(".");
-      }
-    }
-    const finished = Date.now();
-    process.stdout.write("\nTook " + (finished - now) + " ms\n");
-    return hash;
-  }
-
+  constructor (
+    public blocks: IBlock[] = [] ,
+    public genesisBlock: IBlock = new Block()
+  ) {}  
 
 }
 
